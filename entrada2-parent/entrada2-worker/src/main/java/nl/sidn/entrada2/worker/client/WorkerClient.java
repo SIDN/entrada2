7d4d85b99da6e89156c0c1f08810d562e3ac05f9
Maarten <maarten.wullink@sidn.nl>
2023-08-10 14:49:22 +0200

    refactor

--- entrada2-parent/entrada2-worker/src/main/java/nl/sidn/entrada2/worker/client/WorkerClient.java
+++
@@ -1,9 +0,0 @@
-package nl.sidn.entrada2.worker.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import nl.sidn.entrada2.worker.api.BaseWork;
-
-@FeignClient(name="workerClient")
-public interface WorkerClient extends BaseWork {
-
-}
