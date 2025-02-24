57cf522e62f95d3857f8a517874ad907dce5c9cf
Maarten Wullink <maarten.wullink@sidn.nl>
2025-02-24 09:12:22 +0100

    Adding support for RESt catalog see #16

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,5 @@
 echo "Build image for version $TOOL_VERSION"
 
 mvn clean && mvn package &&  \
-  docker build --platform=linux/amd64 --tag=sidnlabs/entrada2:$TOOL_VERSION .
+  docker build --platform=linux/amd64 --tag=sidnlabs/entrada2:$TOOL_VERSION . && \
+  docker push sidnlabs/entrada2:$TOOL_VERSION
--- src/main/java/nl/sidn/entrada2/config/IcebergCatalogConfig.java
+++ src/main/java/nl/sidn/entrada2/config/IcebergCatalogConfig.java
@@ -9,6 +9,8 @@ import org.apache.iceberg.aws.glue.GlueCatalog;
 import org.apache.iceberg.aws.s3.S3FileIOProperties;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.jdbc.JdbcCatalog;
+import org.apache.iceberg.rest.RESTCatalog;
+import org.apache.iceberg.rest.auth.OAuth2Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +26,15 @@ import lombok.extern.slf4j.Slf4j;
 @ConfigurationProperties(prefix = "iceberg.catalog")
 @DependsOn("s3Config")
 public class IcebergCatalogConfig {
+
+	private String type;
+
+	// REST catalog
+	private String uri;
+	private String warehouse;
+	private String oauth2ServerUri;
+	private String oauth2Credential;
+	private String oauth2Scope;
 
 	// jdbc config
 	private String host;
@@ -42,47 +53,71 @@ public class IcebergCatalogConfig {
 
 	@Bean
 	public Catalog catalog() {
-		if (StringUtils.isBlank(host)) {
-			// default is aws glue catalog
-			return glueCatalog();
-		}
 
-		// fallback is jdbccatalog
-		return jdbcCatalog();
+		return switch (StringUtils.lowerCase(type)) {
+		case "rest" -> restCatalog();
+		case "glue" -> glueCatalog();
+		default -> jdbcCatalog();
+		};
+
 	}
-	
+
+	private Catalog restCatalog() {
+
+		Map<String, String> properties = new HashMap<>();
+		properties.put(CatalogProperties.CATALOG_IMPL, "org.apache.iceberg.rest.RESTCatalog");
+
+		properties.put(CatalogProperties.URI, uri);
+		properties.put(CatalogProperties.WAREHOUSE_LOCATION, warehouse);
+
+		properties.put(OAuth2Properties.OAUTH2_SERVER_URI, oauth2ServerUri);
+		properties.put(OAuth2Properties.CREDENTIAL, oauth2Credential);
+		properties.put(OAuth2Properties.SCOPE, oauth2Scope);
+
+		properties.put(S3FileIOProperties.SECRET_ACCESS_KEY, secretKey);
+		properties.put(S3FileIOProperties.ACCESS_KEY_ID, accessKey);
+
+		properties.put(S3FileIOProperties.ENDPOINT, endpoint);
+		properties.put(S3FileIOProperties.PATH_STYLE_ACCESS, "true");
+		properties.put(CatalogProperties.FILE_IO_IMPL, "org.apache.iceberg.aws.s3.S3FileIO");
+
+		RESTCatalog catalog = new RESTCatalog();
+		catalog.initialize("iceberg", properties);
+		return catalog;
+
+	}
+
 	private Catalog jdbcCatalog() {
 		log.info("Creating Iceberg JDBC Catalog");
-		
+
 		Map<String, String> properties = new HashMap<>();
-		properties.put(JdbcCatalog.PROPERTY_PREFIX + "schema-version" , "V1");
-		
+		properties.put(JdbcCatalog.PROPERTY_PREFIX + "schema-version", "V1");
+
 		properties.put(CatalogProperties.CATALOG_IMPL, JdbcCatalog.class.getName());
 		properties.put(CatalogProperties.URI, "jdbc:postgresql://" + host + ":" + port + "/" + name);
 		properties.put(JdbcCatalog.PROPERTY_PREFIX + "user", user);
 		properties.put(JdbcCatalog.PROPERTY_PREFIX + "password", password);
-		
+
 		properties.put(CatalogProperties.FILE_IO_IMPL, "org.apache.iceberg.aws.s3.S3FileIO");
-		
+
 		properties.put(CatalogProperties.WAREHOUSE_LOCATION, warehouseLocation);
-		
+
 		if (StringUtils.isNotBlank(endpoint)) {
-		   properties.put(S3FileIOProperties.ENDPOINT, endpoint);
+			properties.put(S3FileIOProperties.ENDPOINT, endpoint);
 		}
-		
+
 		properties.put(S3FileIOProperties.SECRET_ACCESS_KEY, secretKey);
 		properties.put(S3FileIOProperties.ACCESS_KEY_ID, accessKey);
 		properties.put(S3FileIOProperties.PATH_STYLE_ACCESS, "true");
-		
+
 		JdbcCatalog catalog = new JdbcCatalog();
 		catalog.initialize("iceberg", properties);
 		return catalog;
 	}
 
-
 	private Catalog glueCatalog() {
 		log.info("Creating Iceberg AWS Glue Catalog");
-		
+
 		Map<String, String> properties = new HashMap<>();
 		properties.put(CatalogProperties.CATALOG_IMPL, "org.apache.iceberg.aws.glue.GlueCatalog");
 
@@ -91,7 +126,7 @@ public class IcebergCatalogConfig {
 		properties.put(S3FileIOProperties.PATH_STYLE_ACCESS, "true");
 
 		properties.put("http-client.apache.connection-timeout-ms", "10000");
-		properties.put("http-client.apache.socket-timeout-ms","10000");
+		properties.put("http-client.apache.socket-timeout-ms", "10000");
 
 		GlueCatalog catalog = new GlueCatalog();
 		catalog.initialize("iceberg", properties);
