a08abd8b982d26bb4aa3d560188ac7ab1a44310c
Maarten Wullink <maarten.wullink@sidn.nl>
2026-04-17 10:19:50 +0200

    Fixes #38

--- src/main/java/nl/sidn/entrada2/Application.java
+++ src/main/java/nl/sidn/entrada2/Application.java
@@ -2,16 +2,22 @@ package nl.sidn.entrada2;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @EnableFeignClients
 @EnableAsync
@@ -19,14 +25,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableDiscoveryClient
 public class Application {
-	
-
-  @Value("${spring.application.name}")
-  private String appName;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Bean
+  public ApplicationRunner logStartup(BuildProperties buildProperties) {
+    return args -> log.info("Starting {} version {}", buildProperties.getName(), buildProperties.getVersion());
+  }
 
 }
